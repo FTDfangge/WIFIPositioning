4d66fbef790926d19e15e57e034bbe70c59913cc
zzzzffff6666 <64240728+zzzzffff6666@users.noreply.github.com>
2020-07-01 15:05:56 +0800

    Update PositionClient.java

--- Basic_Function/network/PositionClient/PositionClient.java
+++ Basic_Function/network/PositionClient/PositionClient.java
@@ -7,15 +7,16 @@ import java.net.*;
  * 邮箱：18301040@bjtu.edu.cn
  **/
 public class To_Server implements Runnable{
-    static Socket client;
-    BufferedReader bufferedReader;
-    static public PrintWriter writer;
+    private static Socket client;
+    private BufferedReader bufferedReader;
+    private static public PrintWriter writer;
 
     public void send_to_server(String message){
 
         writer.println(message);
         writer.flush();
     }
+    
     public String recive_server(){
         String x="";
         try {
@@ -28,9 +29,10 @@ public class To_Server implements Runnable{
         }
         return x;
     }
-public void run()
-{
-     try {
+    
+    public void run()
+    {
+        try {
             client = new Socket("", 2000);
             writer = new PrintWriter(client.getOutputStream(), true);
             writer.flush();
@@ -38,19 +40,6 @@ public void run()
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 }
-    
-  /*  public void connect(String[] args) {
-        String serverName = args[0];
-        int port = Integer.parseInt(args[1]);
-        try {
-            client = new Socket(serverName, port);
-
-            writer = new PrintWriter(client.getOutputStream(), true);
-            writer.flush();
-            System.out.println("successful");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
- }*/
 
