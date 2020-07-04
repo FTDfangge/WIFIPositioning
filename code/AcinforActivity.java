57d5d6fe83b3de4234e3e30e41d738583fe3accd
FTDfangge <44743844+FTDfangge@users.noreply.github.com>
2020-07-04 20:52:28 +0800

    Update AcinforActivity.java
    Updated 07-04 version, we finished wifi scanning part

--- code/AcinforActivity.java
+++ code/AcinforActivity.java
@@ -1,15 +1,31 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AcinforActivity extends AppCompatActivity {
 
+    Button returnbtn12;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accountinfor_layout);
+
+        returnbtn12 = (Button) findViewById(R.id.returnbtn12);
+
+        returnbtn12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+
 
     }
 }
