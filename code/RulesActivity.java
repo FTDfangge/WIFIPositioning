ea293cc694a38eb4f9b86fe4fad8b26f8b851ebf
FTDfangge <44743844+FTDfangge@users.noreply.github.com>
2020-07-04 21:13:49 +0800

    Create RulesActivity.java
    07-04 version ,finished the wifi scan part

---
+++ code/RulesActivity.java
@@ -0,0 +1,28 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RulesActivity extends AppCompatActivity{
+
+    Button returnbtn6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rules_layout);
+
+        returnbtn6 = (Button) findViewById(R.id.returnbtn6);
+
+        returnbtn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+
+    }
+}
