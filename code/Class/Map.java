dde73fb5b4a473b3f8e6069b0eedd446afe3813c
FTDfangge <44743844+FTDfangge@users.noreply.github.com>
2020-07-05 03:08:55 +0800

    Created map class

---
+++ code/Class/Map.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Map {
+    //singleton version
+    private  Map map;
+    private  List<Point> pointList;
+    private String id;
+
+    private Map() {
+
+    }
+
+    public void setPoints(List<Point> pList)
+    {
+        this.pointList = pList;
+    }
+
+    public void setID(String id)
+    {
+        this.id = id;
+    }
+
+    public List<Point> getPointList()
+    {
+        return pointList;
+    }
+
+    public String getID()
+    {
+        return id;
+    }
+
+}
