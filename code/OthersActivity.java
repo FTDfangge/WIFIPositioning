a8eec89a99ced93f5179c53e618b1ee26c6b369e
FTDfangge <44743844+FTDfangge@users.noreply.github.com>
2020-07-04 21:10:53 +0800

    Create OthersActivity.java
    07-04 version ,finished the wifi scan part

---
+++ code/OthersActivity.java
@@ -0,0 +1,29 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OthersActivity extends AppCompatActivity {
+
+    Button returnbtn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.others_layout);
+
+        returnbtn3 = (Button) findViewById(R.id.returnbtn3);
+
+        returnbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+
+
+    }
+}
