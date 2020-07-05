8575d9a5f486aed1d5b60f9b41c2e8a0bf61d3e8
FTDfangge <44743844+FTDfangge@users.noreply.github.com>
2020-07-05 14:54:09 +0800

    Add positionerByDis and positionerByCos

--- code/LocationActivity.java
+++ code/LocationActivity.java
@@ -1,12 +1,21 @@
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.schoolwifipositioning.CLASSES.Point;
+import com.example.schoolwifipositioning.CLASSES.Positioning;
+import com.example.schoolwifipositioning.CLASSES.WIFIScanner;
 
 public class LocationActivity extends AppCompatActivity {
 
     Button returnbtn9;
+    ImageButton positionButton,positionButton2;
+    Point currentPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,30 @@ public class LocationActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 finish();
+
+            }
+        });
+        
+        positionButton = (ImageButton) findViewById(R.id.imageButton);
+        positionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPoint.setAPList(WIFIScanner.getInstance().getResultList());
+
+                Positioning.positionTool().getMyPositionByDis(currentPoint);
+
+
+            }
+        });
+
+        positionButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        positionButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPoint.setAPList(WIFIScanner.getInstance().getResultList());
+
+                Positioning.positionTool().getMyPositionByCos(currentPoint);
+
 
             }
         });
